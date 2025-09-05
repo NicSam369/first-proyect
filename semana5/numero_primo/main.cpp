f9d1977859a62b6e541e4cc5fd612594a5f264b5
Sky <scubidoo468@gmail.com>
2025-09-04 22:29:34 -0500

    Terminado numeros primos

--- semana5/numero_primo/main.cpp
+++ semana5/numero_primo/main.cpp
@@ -8,40 +8,47 @@ int main()
     e indique si dicjo numero es un numero primo
     */
     int num;
+    int c = 0;
+    int d = 1;
     cout << "Ingrese un numero: ";
     cin >> num;
 
 
-    while (divisor < num){
-    /**    divisor = divisor + 1;
-        if (num % divisor == 0){
-            cout << num << "No es un numero primo";
-            break
-        } else {
-            cout << num << " es un numero primo"
+    while (d <= num){
+        if (num % d == 0){
+            c = c + 1;
         }
+        d = d +1;
     }
-    */
+    if (c==2) {
+        cout << num << " es un numero primo" << endl;
+    } else {
+        cout << num << " no es un numero primo" << endl;
+    }
 
     /**
     Implemente un programa que solicite un numero por teclado
     e imprime los numeros primos menores que dicho numero
     */
-    int numero;
-    cout << "Ingrese un numero: ";
-    cin >> numero;
+    int n = 2;
+    d=1;
+    c=0;
 
-    int d=1, c = 1;
-
-    while ((c <= numero ) && (numero >= 0)) {
-        if ( numero % d == 0)
-            cout << numero << " es un numero primo" << " ";
+    while (n<=num){
+        d = 1;
+        c= 0;
+        while (d<=n){
+            if (n%d==0){
+                c=c+1;
+            }
+            d = d + 1;
         }
-        d = d + 1
+        if (c==2){
+            cout << n << " ";
+        }
+        n = n + 1;
 
-    /**
-    Tarea para el viernes
-    */
+    }
 
     return 0;
 }
